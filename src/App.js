a8ce4876067a216172baa2e411d4dce6bb579aea
SimonGPort <simon.giroux.portelance@gmail.com>
2021-07-29 17:06:56 -0400

    sass push

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+// import './App.css';
+import './styles/style.css'
+// Tutorial youtube:
+// https://youtu.be/Zz6eOVaaelI
+// https://youtu.be/roywYSEPSvc
+
+function App() {
+  return (<>
+<header>
+<h1>Hello my dear</h1>
+<button>Hello my dear</button>
+</header>
+
+<div class="contact">
+<button>Submit</button>
+<div class="info">
+<h1>Our contact info</h1>
+<p>Our contact info</p>
+</div>
+</div>
+
+  </>
+  );
+}
+
+export default App;
